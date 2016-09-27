ba8c934122f07482d1fed7ec1f551d6d9b238a08
Kasper <kasperlaugemadsen@gmail.com>
2016-09-27 10:05:04 +0200

    Ændring af offset

--- Exercise8/file_client/file_client.cs
+++ Exercise8/file_client/file_client.cs
@@ -56,13 +56,11 @@ namespace tcp
 			// read the file in chunks of 1000Bytes
 			var buffer = new byte[BUFSIZE];
 			int bytesRead = io.Read (buffer, 0, BUFSIZE);
-			var byteSum = bytesRead;
 
 			while (bytesRead > 0) 
 				{
-					file.Write (buffer, 0, byteSum);
+					file.Write (buffer, 0, bytesRead);
 					bytesRead = io.Read(buffer,0,BUFSIZE);
-					byteSum += bytesRead;
 				}
 
 
