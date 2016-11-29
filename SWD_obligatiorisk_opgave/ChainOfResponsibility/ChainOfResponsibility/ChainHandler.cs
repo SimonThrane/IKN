852c3f9cb2d4d739cf45d06e94189a349eb48f60
Lars <hjerrildlars@gmail.com>
2016-11-29 11:54:20 +0100

    initial

---
+++ SWD_obligatiorisk_opgave/ChainOfResponsibility/ChainOfResponsibility/ChainHandler.cs
@@ -0,0 +1,31 @@
+﻿namespace ChainOfResponsibility
+{
+    public abstract class ChainHandler<T>
+    {
+        public ChainHandler<T> NextChain { get; set; }
+
+        public void SetNextChain(ChainHandler<T> nextChain)
+        {
+            NextChain = nextChain;
+        }
+
+        public abstract T Parse(string inputdata, string inputtype);
+
+    }
+
+    class XMLParser<T> : ChainHandler<T>
+    {
+        public override T Parse(string inputdata, string inputtype)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    class JsonParser<T> : ChainHandler<T>
+    {
+        public override T Parse(string inputdata, string inputtype)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
