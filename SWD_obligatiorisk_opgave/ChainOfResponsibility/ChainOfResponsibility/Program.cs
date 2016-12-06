e1c4f37365825a5ca9ea0883b367f137be609d23
Lars Hjerrild <201409555@post.au.dk>
2016-12-06 10:56:37 +0100

    changes

--- SWD_obligatiorisk_opgave/ChainOfResponsibility/ChainOfResponsibility/Program.cs
+++ SWD_obligatiorisk_opgave/ChainOfResponsibility/ChainOfResponsibility/Program.cs
@@ -5,6 +5,8 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace ChainOfResponsibility
 {
@@ -20,6 +22,8 @@ namespace ChainOfResponsibility
             handler1.SetNextChain(handler2);
             handler2.SetNextChain(handler3);
 
+            XmlSerializer xs = new XmlSerializer(typeof(Car));
+
             //Generate data.
 
             var c = new Car();
@@ -27,9 +31,43 @@ namespace ChainOfResponsibility
             c.Model = "Audi";
             c.Owner = "Casper";
 
-            List<Data> data = new List<Data>();
+            var c1 = new Car();
+            c.Age = 3;
+            c.Model = "VW";
+            c.Owner = "Simon";
+
+            var c2 = new Car();
+            c.Age = 8;
+            c.Model = "BMW";
+            c.Owner = "Lars";
+
+            string xml1;
+
+            List <Data> data = new List<Data>();
+
+            data.Add(new ChainOfResponsibility.Data
+            {
+                InputData = JsonConvert.SerializeObject(c),
+                InputType = "json"
+            });
+
+            using (var sw = new StringWriter())
+            {
+                using (XmlWriter writer = XmlWriter.Create(sw))
+                {
+                    xs.Serialize(writer, c1);
+                    xml1 = sw.toString();
+                }
+            }
+
+                 
 
 
+            data.Add(new ChainOfResponsibility.Data
+            {
+                
+                InputData = xs.Serialize()
+            })
             
 
 
